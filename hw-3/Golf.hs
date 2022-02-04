57aff0503ecf534d08e5cf87d50fb1ed182c01d0
Brandon Ling <brandonvling@gmail.com>
2022-02-04 09:42:35 -0500

    Complete exercise 2 of assignment 2

--- hw-3/Golf.hs
+++ hw-3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List
+
 skipN :: Int -> [a] -> [a]
 skipN _ [] = []
 skipN 0 as = as
@@ -11,3 +13,22 @@ skips :: [a] -> [[a]]
 skips as = map (`skipN` as) [0..n - 1]
   where n = length as 
 
+isLocalMaximum :: (Integer, Integer, Integer) -> Bool
+isLocalMaximum (left, mid, right) = mid > left && mid > right
+
+-- windows takes a list of integers and returns a list of tuples
+-- that represent the sliding windows to check for local maxima
+windows :: [Integer] -> [(Integer, Integer, Integer)]
+windows [] = []
+windows [_] = []
+windows [_, _] = []
+windows xs = (left, mid, right) : windows (drop 1 xs)
+  where [left, mid, right] = take 3 xs
+
+-- localMaxima first filters out non local maxima windows and then 
+-- extracts the maximum from each window
+localMaxima :: [Integer] -> [Integer]
+localMaxima [] = []
+localMaxima [_] = []
+localMaxima [_, _] = []
+localMaxima xs = map (\(left, mid, right) -> mid) $ filter isLocalMaximum (windows xs)
