b29418a36da79ede3bd301d1535333f73432d172
Brandon Ling <brandonvling@gmail.com>
2022-02-07 09:44:01 -0500

    Add balanced binary tree solution
    Exercise 2 assignment 4

--- hw-4/Solution.hs
+++ hw-4/Solution.hs
@@ -25,3 +25,45 @@ transform n
 fun2' :: Integer -> Integer
 fun2' n = sum $ filter even fun2Sequence
   where fun2Sequence = takeWhile (/= 1) (iterate transform n)
+
+-- Exercise 2
+data Tree a = Leaf
+            | Node Integer (Tree a) a (Tree a)
+  deriving (Show, Eq)
+
+embed :: a -> Tree a
+embed a = Node 0 Leaf a Leaf
+
+heightTree :: Tree a -> Integer
+heightTree Leaf = -1
+heightTree (Node height _ _ _) = height
+
+addNode :: a -> Tree a -> Tree a
+addNode a Leaf = embed a
+addNode a (Node height leftTree val rightTree)
+  | leftHeight > rightHeight = Node height leftTree val newRightSubtree
+  | rightHeight > leftHeight = Node height newLeftSubtree val rightTree
+  | newLeftHeight == leftHeight = Node height newLeftSubtree val rightTree
+  | newRightHeight == rightHeight = Node height leftTree val newRightSubtree
+  | otherwise = Node (height + 1) newLeftSubtree val rightTree 
+  where newRightSubtree = addNode a rightTree
+        newRightHeight = heightTree newRightSubtree
+        newLeftSubtree = addNode a leftTree
+        newLeftHeight = heightTree newLeftSubtree
+        leftHeight = heightTree leftTree
+        rightHeight = heightTree rightTree
+
+
+foldTree :: [a] -> Tree a
+foldTree [] = Leaf
+foldTree as = foldr addNode Leaf as
+
+isValidBalancedTree :: Tree a -> Bool
+isValidBalancedTree Leaf = True
+isValidBalancedTree (Node height left _ right) =  
+  (height == leftHeight + 1 || height == rightHeight + 1)
+  && abs (leftHeight - rightHeight) <= 1 
+  && isValidBalancedTree left 
+  && isValidBalancedTree right
+  where leftHeight = heightTree left
+        rightHeight = heightTree right
