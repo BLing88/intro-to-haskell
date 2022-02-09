ee0f919ae30469f529940d41e94d3c569a39dc31
Brandon Ling <brandonvling@gmail.com>
2022-02-08 23:01:37 -0500

    Implement evaluator for a string

--- hw-5/Calc.hs
+++ hw-5/Calc.hs
@@ -1,9 +1,16 @@
 module Calc where
 
 import ExprT
+import Parser (parseExp)
 
 -- Exercise 1
 eval :: ExprT -> Integer
 eval (Lit n) = n
 eval (Add expr1 expr2) = eval expr1 + eval expr2
 eval (Mul expr1 expr2) = eval expr1 * eval expr2
+
+-- Exercise 2
+evalStr :: String -> Maybe Integer
+evalStr s = case parseExp Lit Add Mul s of
+  Just expr -> Just (eval expr)
+  Nothing -> Nothing
