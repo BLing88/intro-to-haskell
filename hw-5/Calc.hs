9056ab214f75b475fdf2ede16aea5a9a0066e902
Brandon Ling <brandonvling@gmail.com>
2022-02-13 20:12:53 -0500

    Write Expr instance for Program and compile function
    Exercise 5 assignment 5

--- hw-5/Calc.hs
+++ hw-5/Calc.hs
@@ -1,7 +1,9 @@
+{-# LANGUAGE FlexibleInstances #-}
 module Calc where
 
 import ExprT
 import Parser (parseExp)
+import qualified StackVM as SVM
 
 -- Exercise 1
 eval :: ExprT -> Integer
@@ -12,3 +14,73 @@ eval (Mul expr1 expr2) = eval expr1 * eval expr2
 -- Exercise 2
 evalStr :: String -> Maybe Integer
 evalStr s = eval <$> parseExp Lit Add Mul s 
+
+-- Exercise 3
+class Expr a where
+  lit :: Integer -> a
+  mul :: a -> a -> a 
+  add :: a -> a -> a
+
+instance Expr ExprT where
+  lit = Lit
+  add = Add
+  mul = Mul
+
+-- Exercise 4
+
+instance Expr Integer where
+  lit = id
+  add = (+)
+  mul = (*)
+
+instance Expr Bool where
+  lit n = n > 0 
+  add b1 b2 = b1 || b2
+  mul b1 b2 = b1 && b2
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax 
+  add (MinMax m) (MinMax n) = MinMax (max m n)
+  mul (MinMax m) (MinMax n) = MinMax (min m n)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit n = Mod7 (n `mod` 7)
+  add (Mod7 m) (Mod7 n) = Mod7 $ (m `mod` 7) + (n `mod` 7)
+  mul (Mod7 m) (Mod7 n) = Mod7 $ ((m `mod` 7) * (n `mod` 7)) `mod` 7
+
+testExpr :: Expr a => Maybe a
+testExpr = parseExp lit add mul "(3 * -4) + 5"
+
+testInteger = testExpr :: Maybe Integer
+testBool = testExpr :: Maybe Bool
+testMinMax = testExpr :: Maybe MinMax
+testMod7 = testExpr :: Maybe Mod7
+
+-- Exercise 5
+instance Expr SVM.Program where
+  lit n = [SVM.PushI n]
+  add p1 p2 = case (result1, result2) of
+    (Right (SVM.IVal m), Right (SVM.IVal n)) -> [SVM.PushI $ add m n]
+    (_, _) -> []
+    where result1 = SVM.stackVM p1
+          result2 = SVM.stackVM p2
+  mul p1 p2 =  case (result1, result2) of
+    (Right (SVM.IVal m), Right (SVM.IVal n)) -> [SVM.PushI $ mul m n]
+    (_, _) -> []
+    where result1 = SVM.stackVM p1
+          result2 = SVM.stackVM p2
+
+compile :: String -> Maybe SVM.Program
+compile = parseExp lit add mul
+
+x :: Either String SVM.StackVal
+x = SVM.stackVM (add (lit 4) (mul (lit (-9)) (lit (-2)))) -- Right (IVal 22)
+
+expr :: Expr a => a
+expr = mul (add (lit 4) (mul (lit (-1)) (lit 3))) (mul (lit 5) (lit 8))
+
+test = SVM.stackVM expr -- Right (SVM.IVal 40))
